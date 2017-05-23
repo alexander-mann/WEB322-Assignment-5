357cfcb2750051e972788b849ff1b85247612cc6
alexander-mann <amann9@myseneca.ca>
2017-05-23 10:51:05 -0400

    updated server.js - installed path.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 
 var HTTP_PORT = process.env.PORT || 8080;
@@ -10,13 +11,16 @@ function onHttpStart() {
 
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
-   res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
+   res.sendFile(path.join(__dirname + "/views/home.html"));
 });
 
 // setup another route to listen on /about
 app.get("/about", function(req,res){
-   res.send("<h3>About</h3>");
+   res.sendFile(path.join(__dirname + "/views/about.html"));
 });
+
+// used for css implimentation - to be discussed in class at later date
+app.use(express.static('public'));
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
