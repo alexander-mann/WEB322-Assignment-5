01f5890abec0c42c31839193d0e9376c52b5fd49
alexander-mann <amann9@myseneca.ca>
2017-06-07 13:09:52 -0400

    initialize()

--- data-service.js
+++ data-service.js
@@ -1,3 +1,27 @@
 var employees = [];
 var departments = [];
 
+const fs = require('fs');
+
+module.exports.initialize = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/employees.json', (err, data) => {
+            if(err) {
+                reject("Initialization Error - Employee Data");
+            }
+            else {
+                employees = JSON.parse(data);
+                fs.readFile('./data/departments.json', (err, data) => {
+                    if(err) {
+                        reject("Initialization Error - Department Data");
+                    }
+                    else {
+                        departments = JSON.parse(data);
+                        resolve("Initialization Successful");
+                    }
+                });
+            }
+        });
+    });
+};
+
