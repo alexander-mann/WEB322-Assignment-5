5cbca4ef6cb4d2b2869d2fbcf8332bf101b1fb28
alexander-mann <amann9@myseneca.ca>
2017-06-20 21:55:58 -0400

    4.7.2

--- data-service.js
+++ data-service.js
@@ -186,10 +186,12 @@ module.exports.addEmployee = (employeeData) => {
 // FUNCTION: UPDATE EMPLOYEE INFORMATION
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.updateEmployee = (employeeData) => {
+    console.log("Manager? " + employeeData.isManager);
     return new Promise((resolve, reject) => {
         for (let i = 0; i < employees.length; i++) {
             if (employees[i].employeeNum == employeeData.employeeNum) {
                 employees[i] = employeeData;
+                console.log("Final = " + employees[i].isManager);
                 resolve();
             }
         }
