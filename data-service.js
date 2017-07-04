439df02ec818a275626a3b9a4ccf8a1021b61163
alexander-mann <amann9@myseneca.ca>
2017-07-04 18:49:13 -0400

    5.0

--- data-service.js
+++ data-service.js
@@ -1,200 +1,125 @@
 ////////////////////////////////////////////////////////////////////////////////
-// ARRAYS FOR DATA STORAGE
+// ASSIGNMENT 5 ADD-ONS
 ////////////////////////////////////////////////////////////////////////////////
-var employees = [];
-var departments = [];
-var empCount = 0;
-////////////////////////////////////////////////////////////////////////////////
-// INCLUDE FS - FILE SYSTEM
-////////////////////////////////////////////////////////////////////////////////
-const fs = require('fs');
+const Sequelize = require('sequelize');
+// set up sequelize to point to our postgres database
+var sequelize = new Sequelize('d7n3rptalm9ma1', 'nyvjasvfjikvpr', 'a7ca25cefdf39388d0ee2eb31a0f1bfd8810b531fd8404e7bead9ec39c3a7792', {
+    host: 'ec2-184-73-249-56.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+        ssl: true
+    }
+});
+// define Employee model
+var Employee = sequelize.define('Employee', {
+    employeeNum: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstName: Sequelize.STRING,
+    last_name: Sequelize.STRING,
+    email: Sequelize.STRING,
+    SSN: Sequelize.STRING,
+    addressStreet: Sequelize.STRING,
+    addressCity: Sequelize.STRING,
+    addressState: Sequelize.STRING,
+    addressPostal: Sequelize.STRING,
+    maritalStatus: Sequelize.STRING,
+    isManager: Sequelize.BOOLEAN,
+    employeeManagerNum: Sequelize.INTEGER,
+    status: Sequelize.STRING,
+    department: Sequelize.INTEGER,
+    hireDate: Sequelize.STRING
+})
+// define Department model
+var Department = sequelize.define('Department', {
+    departmentId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    departmentName: Sequelize.STRING
+})
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION: POPULATE DATA INTO GLOBAL ARRAYS
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.initialize = () => {
-    return new Promise((resolve, reject) => {
-        fs.readFile('./data/employees.json', (err, data) => {
-            if (err) {
-                reject("Initialization Error - Employee Data");
-            }
-            else {
-                employees = JSON.parse(data);
-                // console.log(employees); // test
-                fs.readFile('./data/departments.json', (err, data) => {
-                    if (err) {
-                        reject("Initialization Error - Department Data");
-                    }
-                    else {
-                        departments = JSON.parse(data);
-                        // console.log(departments); // test
-                        empCount = employees.length; // set employee count
-                        resolve("Initialization Successful");
-                    }
-                });
-            }
-        });
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 };
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION: GET ALL EMPLOYEES
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.getAllEmployees = () => {
-    return new Promise((resolve, reject) => {
-        if (employees.length === 0) {
-            reject("No Employee Data Found");
-        }
-        else {
-            console.log("All Employees = " + employees.length); // test
-            resolve(employees);
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 };
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION: GET ALL EMPLOYEES BY STATUS
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.getEmployeesByStatus = (status) => {
-    let empStatus = []; // temp array to hold results
-    for (let i = 0; i < employees.length; i++) {
-        if (employees[i].status == status) {
-            empStatus.push(employees[i]);
-        }
-    }
-    return new Promise((resolve, reject) => {
-        if (empStatus.length === 0) {
-            reject("No Matching Results Found");
-        }
-        else {
-            console.log(status + " Employees = " + empStatus.length); // test
-            resolve(empStatus);
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 };
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION: GET ALL EMPLOYEES BY DEPARTMENT
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.getEmployeesByDepartment = (department) => {
-    let empDept = []; // temp array to hold results
-    for (let i = 0; i < employees.length; i++) {
-        if (employees[i].department == department) {
-            empDept.push(employees[i]);
-        }
-    }
-    return new Promise((resolve, reject) => {
-        if (empDept.length === 0) {
-            reject("No Matching Results Found");
-        }
-        else {
-            console.log("Employees in Department " + department + " = " + empDept.length); // test
-            resolve(empDept);
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 };
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION: GET ALL EMPLOYEES BY MANAGER
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.getEmployeesByManager = (manager) => {
-    let empMngr = []; // temp array to hold results
-    for (let i = 0; i < employees.length; i++) {
-        if (employees[i].employeeManagerNum == manager) {
-            empMngr.push(employees[i]);
-        }
-    }
-    return new Promise((resolve, reject) => {
-        if (empMngr.length === 0) {
-            reject("No Matching Results Found");
-        }
-        else {
-            console.log("Employees with Manager " + manager + " = " + empMngr.length); // test
-            resolve(empMngr);
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 };
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION: GET EMPLOYEE BY ID
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.getEmployeeByNum = (num) => {
-    let emp = []; // temp array to hold results
-    for (let i = 0; i < employees.length; i++) {
-        if (employees[i].employeeNum == num) {
-            emp = employees[i];
-        }
-    }
-    return new Promise((resolve, reject) => {
-        if (emp.length === 0) {
-            reject("No Employee with ID " + num + " Found");
-        }
-        else {
-            console.log("Employee with ID " + num + " = " + emp.firstName + " " + emp.last_name); // test
-            resolve(emp);
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 };
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION: GET MANAGERS
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.getManagers = () => {
-    let mngrs = []; // temp array to hold results
-    for (let i = 0; i < employees.length; i++) {
-        if (employees[i].isManager) {
-            mngrs.push(employees[i]);
-        }
-    }
-    return new Promise((resolve, reject) => {
-        if (mngrs.length === 0) {
-            reject("No Managers Found");
-        }
-        else {
-            console.log("All Managers = " + mngrs.length); // test
-            resolve(mngrs);
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 };
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION: GET ALL DEPARTMENTS
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.getDepartments = () => {
-    return new Promise((resolve, reject) => {
-        if (departments.length === 0) {
-            reject("No Department Data Found");
-        }
-        else {
-            console.log("All Departments = " + departments.length); // test
-            resolve(departments);
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 };
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION: ADD NEW EMPLOYEE
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.addEmployee = (employeeData) => {
-    return new Promise((resolve, reject) => {
-        // increment empCount
-        empCount++;
-        // assign employee number
-        employeeData.employeeNum = empCount;
-        // assign new employee to array
-        employees.push(employeeData);
-        if (employees[empCount - 1].employeeNum != employeeData.employeeNum) {
-            reject("Employee Was Not Added Successfully");
-        }
-        else {
-            resolve();
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 };
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTION: UPDATE EMPLOYEE INFORMATION
 ////////////////////////////////////////////////////////////////////////////////
 module.exports.updateEmployee = (employeeData) => {
-    // console.log("Manager? " + employeeData.isManager);
-    return new Promise((resolve, reject) => {
-        for (let i = 0; i < employees.length; i++) {
-            if (employees[i].employeeNum == employeeData.employeeNum) {
-                employees[i] = employeeData;
-                // console.log("Final = " + employees[i].isManager);
-                resolve();
-            }
-        }
+    return new Promise(function (resolve, reject) {
+        reject();
     });
 };
 ////////////////////////////////////////////////////////////////////////////////
